fb326664e7835c3f4862fe5f114f207c3223055a
Christopher Khoo <khooweiwen@gmail.com>
2016-08-10 10:01:22 +0800

    final

---
+++ emotify.js
@@ -0,0 +1,9 @@
+var emotify = require(process.argv[2])
+console.log(emotify(process.argv[3]))
+
+var isCoolNumber = require(process.argv[2]);
+var assert = require('assert');
+assert(isCoolNumber(42));
+
+//outputs TAP
+//fancify
